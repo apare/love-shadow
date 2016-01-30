16ede1e1db81e589864371328c5b3bb4354c3fa6
Alexandre Paré <alex.pare.inc@gmail.com>
2016-01-30 00:33:00 -0500

    prototype B

--- main.lua
+++ main.lua
@@ -1,23 +1,41 @@
 function love.load()
   img = love.graphics.newImage("a.png")
-  shadowShader = love.graphics.newShader("drawShadow.glsl")
-  shadow = love.graphics.newCanvas(512, 512)--, "r8")
+  lightRaysShader = love.graphics.newShader("lightRays.glsl")
+  shadowShader = love.graphics.newShader("shadow.glsl")
+  computeRaysShader = love.graphics.newShader("computeRays.glsl")
+  
+  lightRays = love.graphics.newCanvas(512, 512, "rg8")
+  shadowRays = love.graphics.newCanvas(512, 512, "rg8")
+  shadow = love.graphics.newCanvas(512, 512, "r8")
+  
   font = love.graphics.newFont("OpenSans.ttf", 20);
+  
   text = love.graphics.newText( font )
 end
 
 
 function drawLight(normalMap, heightMap, length, height)
+  love.graphics.setShader(lightRaysShader)
+  love.graphics.setShader(lightRaysShader)
+  love.graphics.setCanvas(lightRays)
+  love.graphics.draw(heightMap)
+  
   shadowShader:send('lightZ', height)
   love.graphics.setShader(shadowShader)
+  love.graphics.setCanvas(shadowRays)
+  love.graphics.draw(lightRays)
+  
+  love.graphics.setShader(computeRaysShader)
   love.graphics.setCanvas(shadow)
-  love.graphics.draw(heightMap)
+  love.graphics.draw(shadowRays)
+  
   return shadow;
 end
 
+
+
 function love.draw()  
   if(img ~= nil) then
-    local z = (math.sin(love.timer.getTime()) + 1) / 4 + 0.5;
     local time = love.timer.getTime();
     local shadow = drawLight(nil, img, 250, love.mouse.getY() / love.graphics.getHeight())
     for i = 0, 30 do
